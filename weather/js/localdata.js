80b7f782b2442e0e7457bf54a65f6e4aab4d02f1
serahcdp <dor17004@byui.edu>
2019-03-12 23:11:38 -0600

    changes

--- weather/js/localdata.js
+++ weather/js/localdata.js
@@ -11,16 +11,17 @@ let contentContainer = document.getElementById('page-content');
 
 let weatherURL = "js/weather.json";
 fetchData(weatherURL);
-function fetchData(weatherURL){
-    let cityName = 'Greenville'; 
+
+function fetchData(weatherURL) {
+    let cityName = 'Greenville';
     fetch(weatherURL)
-        .then(function(response){
-            if(response.ok){
-            return response.json();
-        }
-        throw new ERROR('Network response was not OK.');
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new ERROR('Network response was not OK.');
         })
-        .then(function(data){
+        .then(function (data) {
             console.log(data);
             let g = data[cityName];
 
@@ -37,7 +38,7 @@ function fetchData(weatherURL){
             let locTemp = g.Temp;
             // Wind Data
             let locWind = g.Wind;
-            console.log('WIND ', + locWind);
+            console.log('WIND ', +locWind);
             // Current conditions
             let locSummary = g.Summary;
 
@@ -50,18 +51,28 @@ function fetchData(weatherURL){
             let lat = g.Latitude;
 
             document.getElementById('longitude').innerHTML = long;
-            
+
             document.getElementById('latitude').innerHTML = lat;
+
+            //gusts
+            let gusts = g.Gusts;
+            document.getElementById('gus').innerHTML = gusts;
+
+
+
+
+
+
             // hourly data
             let locHourly = g.Hourly;
 
             let pageTitle = document.getElementById('page-title');
-            
+
             let fullNameNode = document.createTextNode(fullName);
             pageTitle.insertBefore(fullNameNode, pageTitle.childNodes[0]);
 
             /* HTML */
-            console.log('curtemp1'+ locTemp);
+            console.log('curtemp1' + locTemp);
             // set the location info
 
             // get the h1 to display the city location
@@ -74,9 +85,9 @@ function fetchData(weatherURL){
 
             // wind info
             document.getElementById('speednum').innerHTML = locWind;
-            console.log('speednum ', + locWind);
+            console.log('speednum ', +locWind);
             windDial(g.Direction);
-            console.log("direction: "+g.Direction);
+            console.log("direction: " + g.Direction);
 
             // current conditions info
             document.getElementById('summary-heading').innerHTML = locSummary;
@@ -92,10 +103,8 @@ function fetchData(weatherURL){
             statusContainer.setAttribute('class', 'hide'); // hide status container
         })
         // If there is an error
-        .catch(function(error){
+        .catch(function (error) {
             console.log('There was a fetch problem: ', error.message);
             statusContainer.innerHTML = 'Sorry, the data could not be processed.';
         })
-    }
-
-    
+}
