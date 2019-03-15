907fd03e41c686533975129af06e886ba1ec0a5d
serahcdp <dor17004@byui.edu>
2019-03-14 21:26:11 -0600

    test

--- sandbox/loctest.js
+++ sandbox/loctest.js
@@ -17,18 +17,46 @@ function getGeoLocation() {
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
-         const lat = position.coords.latitude;
-         const long = position.coords.longitude;
-      
-         // Combine the values
-         const locale = lat + "," + long;
-         console.log(`Lat and Long are: ${locale}.`);
-      
-      
+            const lat = position.coords.latitude;
+            const long = position.coords.longitude;
+
+            // Combine the values
+            const locale = lat + "," + long;
+            console.log(`Lat and Long are: ${locale}.`);
+
+
+            // Call getLocation function, send locale
+            getLocation(locale);
         })
-       } else {
+    } else {
         status.innerHTML = "Your browser doesn't support Geolocation or it is not enabled!";
-       } // end else
-      
+    } // end else
 } // end getGeoLocation
 
+// Gets location information from the NWS API
+function getLocation(locale) {
+    const URL = "https://api.weather.gov/points/" + locale; 
+    // NWS User-Agent header (built above) will be the second parameter 
+    fetch(URL, idHeader) 
+    .then(function(response){
+      if(response.ok){ 
+       return response.json(); 
+      } 
+      throw new ERROR('Response not OK.');
+    })
+    .then(function (data) { 
+      // Let's see what we got back
+      console.log('Json object from getLocation function:'); 
+      console.log(data);
+      // Store data to localstorage 
+      storage.setItem("locName", data.properties.relativeLocation.properties.city); 
+      storage.setItem("locState", data.properties.relativeLocation.properties.state); 
+   
+      // Next, get the weather station ID before requesting current conditions 
+      // URL for station list is in the data object 
+      let stationsURL = data.properties.observationStations; 
+      // Call the function to get the list of weather stations
+      //getStationId(stationsURL); 
+     }) 
+    .catch(error => console.log('There was a getLocation error: ', error)) 
+   } // end getLocation function
