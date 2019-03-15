45f6eac27ea29b7f496dba976c455c4563c242fe
serahcdp <dor17004@byui.edu>
2019-03-14 21:03:33 -0600

    sandbox

--- sandbox/loctest.js
+++ sandbox/loctest.js
@@ -13,5 +13,22 @@ getGeoLocation();
 function getGeoLocation() {
     const status = document.getElementById('status');
     status.innerHTML = 'Getting Location...';
+
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+         const lat = position.coords.latitude;
+         const long = position.coords.longitude;
+      
+         // Combine the values
+         const locale = lat + "," + long;
+         console.log(`Lat and Long are: ${locale}.`);
+      
+      
+        })
+       } else {
+        status.innerHTML = "Your browser doesn't support Geolocation or it is not enabled!";
+       } // end else
+      
 } // end getGeoLocation
 
